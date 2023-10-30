0e7e18031e196fc2b619e1fe80221cbf93cc719b
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-10-30 10:15:28 +0100

    provider fix

--- lib/presentation/Widgets/selected_course_topic_card.dart
+++ lib/presentation/Widgets/selected_course_topic_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:wordy/const/app_router.dart';
+import 'package:wordy/const/enums.dart';
 import 'package:wordy/const/urls.dart';
 import 'package:wordy/domain/models/active_course.dart';
 import 'package:wordy/domain/models/progress_in_topic.dart';
@@ -14,7 +16,8 @@ import '../../const/consts.dart';
 import '../../domain/models/topic.dart';
 
 class SelectedCourseTopicCard extends StatefulWidget {
-  SelectedCourseTopicCard({super.key, 
+  SelectedCourseTopicCard({
+    super.key,
     required this.progress,
     required this.beforeQuiz,
     required this.topic,
@@ -34,8 +37,8 @@ class _SelectedCourseTopicCardState extends State<SelectedCourseTopicCard>
   Animation? _animation;
   @override
   void initState() {
-    _animationController =
-        AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
 
     ActiveCourse? afterQuiz = locator<CourseProgressTracker>().afterQuiz;
     if (widget.beforeQuiz != null && afterQuiz != null) {
@@ -66,110 +69,115 @@ class _SelectedCourseTopicCardState extends State<SelectedCourseTopicCard>
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        var quizType = locator<CourseProgressTracker>().quizType;
+    return Consumer<CourseProgressTracker>(
+      builder: (context, model, child) => GestureDetector(
+        onTap: () {
+          var quizType = model.quizType;
 
-        if (quizType == 'Learning' &&
-            widget.progress.knownWords < widget.progress.wordsCount) {
-          context.pushNamed(AppRouter.quizScreen, extra: {
-            'topic': widget.topic,
-          });
-        } else if (quizType == 'Learning' &&
-            widget.progress.knownWords == widget.progress.wordsCount) {
-          DialogManager.showSuccessDialog(
-              ui_lang[locator<GlobalDataManager>().interfaceLanguage]![
-                  'completed_topic'],
-              ui_lang[locator<GlobalDataManager>().interfaceLanguage]![
-                  'congratulations'],
-              context,
-              () {});
-        } else {
-          context
-              .pushNamed(AppRouter.quizScreen, extra: {'topic': widget.topic});
-        }
-      },
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: const [
-            BoxShadow(
-              color: Color.fromARGB(40, 190, 64, 240),
-              offset: Offset(0, 6),
-              blurRadius: 6.0,
-            )
-          ],
-        ),
-        height: 150,
-        width: 150,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              height: 70,
-              width: 70,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(50),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Color.fromARGB(108, 204, 132, 233),
-                    offset: Offset(0, 6),
-                    blurRadius: 6.0,
-                  )
-                ],
+          if (quizType.name.toLowerCase() ==
+                  QuizType.learning.name.toLowerCase() &&
+              widget.progress.knownWords < widget.progress.wordsCount) {
+            context.pushNamed(AppRouter.quizScreen, extra: {
+              'topic': widget.topic,
+            });
+          } else if (quizType.name.toLowerCase() ==
+                  QuizType.learning.name.toLowerCase() &&
+              widget.progress.knownWords == widget.progress.wordsCount) {
+            DialogManager.showSuccessDialog(
+                ui_lang[locator<GlobalDataManager>().interfaceLanguage]![
+                    'completed_topic'],
+                ui_lang[locator<GlobalDataManager>().interfaceLanguage]![
+                    'congratulations'],
+                context,
+                () {});
+          } else {
+            context.pushNamed(AppRouter.quizScreen,
+                extra: {'topic': widget.topic});
+          }
+        },
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(
+                color: Color.fromARGB(40, 190, 64, 240),
+                offset: Offset(0, 6),
+                blurRadius: 6.0,
+              )
+            ],
+          ),
+          height: 150,
+          width: 150,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color.fromARGB(108, 204, 132, 233),
+                      offset: Offset(0, 6),
+                      blurRadius: 6.0,
+                    )
+                  ],
+                ),
+                child: Center(
+                    child: Image(
+                        image:
+                            NetworkImage(Urls.kImageUrl + widget.topic.image))),
               ),
-              child: Center(
-                  child: Image(
-                      image:
-                          NetworkImage(Urls.kImageUrl + widget.topic.image))),
-            ),
-            Text(
-              ui_lang[locator<GlobalDataManager>().interfaceLanguage]![
-                  'topic_label'][widget.progress.name],
-              textAlign: TextAlign.center,
-              style: Theme.of(context)
-                  .textTheme
-                  .titleMedium!
-                  .copyWith(color: Colors.black45),
-            ),
-            RichText(
-                text: TextSpan(
-              children: [
-                TextSpan(
-                  text: _animation != null
-                      ? '${_animation!.value} /'
-                      : "${widget.progress.knownWords} / ",
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleLarge!
-                      .copyWith(color: Colors.black45),
-                ),
-                TextSpan(
-                  text: ' ${widget.progress.wordsCount}',
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleSmall!
-                      .copyWith(color: Colors.black45),
-                ),
-              ],
-            )),
-            ProgressionBar(
-              height: 10,
-              gradient: const [
-                Color.fromARGB(255, 99, 155, 252),
-                Color.fromRGBO(158, 149, 248, 1),
-              ],
-              nonProgressionColor: Colors.white,
-              progress: _animation != null
-                  ? (_animation!.value / widget.progress.wordsCount) * 100
-                  : (widget.progress.knownWords / widget.progress.wordsCount) *
-                      100,
-              width: 150,
-            )
-          ],
+              Text(
+                ui_lang[locator<GlobalDataManager>().interfaceLanguage]![
+                    'topic_label'][widget.progress.name],
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium!
+                    .copyWith(color: Colors.black45),
+              ),
+              RichText(
+                  text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: _animation != null
+                        ? '${_animation!.value} /'
+                        : "${widget.progress.knownWords} / ",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(color: Colors.black45),
+                  ),
+                  TextSpan(
+                    text: ' ${widget.progress.wordsCount}',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleSmall!
+                        .copyWith(color: Colors.black45),
+                  ),
+                ],
+              )),
+              ProgressionBar(
+                height: 10,
+                gradient: const [
+                  Color.fromARGB(255, 99, 155, 252),
+                  Color.fromRGBO(158, 149, 248, 1),
+                ],
+                nonProgressionColor: Colors.white,
+                progress: _animation != null
+                    ? (_animation!.value / widget.progress.wordsCount) * 100
+                    : (widget.progress.knownWords /
+                            widget.progress.wordsCount) *
+                        100,
+                width: 150,
+              )
+            ],
+          ),
         ),
       ),
     );
